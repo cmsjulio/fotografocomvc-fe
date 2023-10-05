6e9a2d499a8682c9fa600948ec2721f02c2dc89c
cmsjulio <cms.julio1@gmail.com>
2023-10-05 04:48:21 -0300

    feat: :construction: dialog via css, html e ts puros

--- src/app/board-user/board-user.component.ts
+++ src/app/board-user/board-user.component.ts
@@ -20,5 +20,39 @@ export class BoardUserComponent implements OnInit {
         this.content = JSON.parse(err.error).message;
       }
     });
+    // Get modal
+  var modal = document.getElementById("myModal");
+  if (modal != null) {
+    modal.style.background = "rgba(0, 0, 0, 0.8)"
   }
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+  if (btn != null) {
+    // btn.style.backgroundColor = 'bl';
+    // btn.style.color = 'white';
+    // btn.style.fontSize = '2em';
+  }
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal
+  btn!.onclick = function () {
+    modal!.style.display = "block";
+  }
+  // When the user clicks on <span> (x), close the modal
+  // span!.onclick = function() {
+  //   modal.style.display = "none";
+  // }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal!.style.display = "none";
+    }
+  }
+  }
+  // Get the modal
+
 }
